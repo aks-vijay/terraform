c651429a6303425ec76473a2d9a78078b2955a77
Akshay V <83549571+aks-vijay@users.noreply.github.com>
2024-01-31 07:56:02 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,3 +21,29 @@ resource "azurerm_resource_group" "appgrp" {
   name     = "app-grp"
   location = "East US"
 }
+
+resource "azurerm_storage_account" "appstorage5695" {
+  name                     = "appstorage5695"
+  resource_group_name      = "app-grp"
+  location                 = "East US"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind = "StorageV2"
+  depends_on = [ azurerm_resource_group.appgrp ]
+}
+
+resource "azurerm_storage_container" "data" {
+  name                  = "data"
+  storage_account_name  = "appstorage5695"
+  container_access_type = "blob"
+  depends_on = [ azurerm_storage_account.appstorage5695 ]
+}
+
+resource "azurerm_storage_blob" "maintf" {
+  name                   = "main.tf"
+  storage_account_name   = "appstorage5695"
+  storage_container_name = "data"
+  type                   = "Block"
+  source                 = "main.tf"
+  depends_on = [ azurerm_storage_container.data ]
+}
